6d0d628c4d82a7d7390213501137f48766c9f68a
Valmir Memeti <valmirmemeti@gmail.com>
2018-04-24 09:23:06 +0200

    add necessary files for boilerplate

--- tests/AdderTest.php
+++ tests/AdderTest.php
@@ -6,8 +6,7 @@ use PHPUnit\Framework\TestCase;
 
 class AdderTest extends TestCase
 {
-    public function testAdd()
-    {
+    public function testAdd(){
         $this->assertEquals(5, Adder::add(2, 3));
     }
 }
